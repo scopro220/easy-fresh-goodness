394bd4644a42f7f5795fd4bc4d266c69913690ac
Scott <scott.provence@yahoo.com>
2021-03-11 17:04:43 -0500

    fixed DetailCard component

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -6,7 +6,7 @@ const LandingPage = () => {
     <div className="landing-page">
       <DetailCard text="Don't know what to make for dinner tonight..." />
       <DetailCard text="...in the mood for some cake or cookies?" />
-      <DetailCard text="You found the right place...<span>Easy Fresh Goodness</span>" />
+      <DetailCard text="You found the right place...Easy Fresh Goodness" />
       <DetailCard text="Go search for all meals, with nutrition, ingredients and instructions." />
       <DetailCard text="Use the quick search, or check the Daily Meal Theme for ideas for any day! ENJOY!!" />
     </div>
