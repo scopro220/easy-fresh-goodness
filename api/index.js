c85ccde097afb18482675aa53fc6723e5a696f53
scott <scott.provence@yahoo.com>
2022-08-29 20:31:46 -0400

    update index.js express

--- api/index.js
+++ api/index.js
@@ -1,8 +1,36 @@
 const express = require("express");
-
 const app = express();
+require('dotenv').config()
+const fetch = require("node-fetch");
+const keys = require("./keys");
 
-require("./searchRoutes")(app);
+
+app.get("/api/search", (req, res) => {
+  const searchQuery = req.query;
+
+  let end = "";
+
+  for (const k in searchQuery) {
+    end = end + `&${k}=${searchQuery[k]}`;
+  }
+  const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${keys.spoonacular_API}&number=100${end}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      res.send(data);
+    });
+});
+
+app.get("/api/recipe/:recipeid", (req, res) => {
+  const id = req.params.recipeid;
+
+  const url = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${keys.spoonacular_API}&includeNutrition=true`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      res.send(data);
+    });
+});
 
 // if (process.env.NODE_ENV === "production") {
 //   app.use(express.static("build"));
@@ -15,5 +43,3 @@ require("./searchRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-module.exports = app
--- api/searchRoutes.js
+++ api/searchRoutes.js
@@ -1,32 +1,32 @@
-require('dotenv').config()
-const fetch = require("node-fetch");
-const keys = require("./keys");
+// require('dotenv').config()
+// const fetch = require("node-fetch");
+// const keys = require("./keys");
 
-module.exports = (app) => {
-  app.get("/api/search", (req, res) => {
-    const searchQuery = req.query;
+// module.exports = (app) => {
+  // app.get("/api/search", (req, res) => {
+  //   const searchQuery = req.query;
 
-    let end = "";
+  //   let end = "";
 
-    for (const k in searchQuery) {
-      end = end + `&${k}=${searchQuery[k]}`;
-    }
-    const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${keys.spoonacular_API}&number=100${end}`;
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        res.send(data);
-      });
-  });
+  //   for (const k in searchQuery) {
+  //     end = end + `&${k}=${searchQuery[k]}`;
+  //   }
+  //   const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${keys.spoonacular_API}&number=100${end}`;
+  //   fetch(url)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       res.send(data);
+  //     });
+  // });
 
-  app.get("/api/recipe/:recipeid", (req, res) => {
-    const id = req.params.recipeid;
+  // app.get("/api/recipe/:recipeid", (req, res) => {
+  //   const id = req.params.recipeid;
 
-    const url = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${keys.spoonacular_API}&includeNutrition=true`;
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        res.send(data);
-      });
-  });
-};
+  //   const url = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${keys.spoonacular_API}&includeNutrition=true`;
+  //   fetch(url)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       res.send(data);
+  //     });
+  // });
+// };
