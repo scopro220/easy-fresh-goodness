b038d7e846d5d0b509879a692c3510e09fdf7307
scott <scott.provence@yahoo.com>
2022-08-29 20:22:08 -0400

    update index.js express

--- api/index.js
+++ api/index.js
@@ -4,14 +4,14 @@ const app = express();
 
 require("./searchRoutes")(app);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("build"));
 
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "src", "build", "index.html"));
-  });
-}
+//   const path = require("path");
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "src", "build", "index.html"));
+//   });
+// }
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
